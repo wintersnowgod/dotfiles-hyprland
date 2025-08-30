c7df9cffd7890c6e2ff1d1889fcfcc6b2a89382b
wintersnowgod <git.xerox732@passinbox.com>
2025-08-30 16:53:37 +0800

    add mozilla user.js

---
+++ .mozilla/user.js
@@ -0,0 +1,4 @@
+user_pref("ui.key.menuAccessKey", 0);
+user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
+user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org");
+user_pref("browser.tabs.unloadOnLowMemory", true);
