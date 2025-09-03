1c01e92ce2f0de0386791fc87781b46f29f03bf9
wintersnowgod <git.xerox732@passinbox.com>
2025-09-04 03:49:32 +0800

    waybar fix on screen keyboard module

--- .config/waybar/scripts/on-screen-keyboard.sh
+++ .config/waybar/scripts/on-screen-keyboard.sh
@@ -1,41 +1,46 @@
 #!/bin/sh
-if [ "$1" = "-t" ]; then
+toggle() {
   if command -v wvkbd-mobintl >/dev/null; then
     if pgrep -x wvkbd-mobintl >/dev/null; then
         pkill wvkbd-mobintl
-        echo '{"text":"󰌐", "tooltip": "On Screen Keyboard: OFF"}'
+        echo '{"alt":"off", "tooltip": "On Screen Keyboard: OFF"}'
     elif command -v wvkbd-mobintl >/dev/null; then
         wvkbd-mobintl -L 300 &
-        echo '{"text":"󰌌", "tooltip": "On Screen Keyboard: ON"}'
+        echo '{"alt":"on", "tooltip": "On Screen Keyboard: ON"}'
     fi
   elif command -v squeekboard >/dev/null; then
     if pgrep -x squeekboard >/dev/null; then
-        # If squeekboard is running → stop it, start fcitx5
         pkill squeekboard
         fcitx5 &
-        echo '{"text":"󰌐", "tooltip": "On Screen Keyboard: OFF"}'
+        echo '{"alt":"off", "tooltip": "On Screen Keyboard: OFF"}'
     elif command -v squeekboard >/dev/null; then
-        # Otherwise → stop fcitx5, start squeekboard
         pkill fcitx5
         squeekboard &
-        echo '{"text":"󰌌", "tooltip": "On Screen Keyboard: ON"}'
+        echo '{"alt":"on", "tooltip": "On Screen Keyboard: ON"}'
     fi
+  fi
+}
+
+case "$1" in
+  -t|--toggle)
+    toggle
+    sleep 0.1
     pkill -SIGRTMIN+15 waybar
     exit 0
-  fi
-fi
+    ;;
+esac
 
 # -------- Waybar refresh branch --------
 if command -v wvkbd-mobintl >/dev/null; then
   if pgrep -x wvkbd-mobintl >/dev/null; then
-    echo '{"text":"󰌌", "tooltip": "On Screen Keyboard: ON"}'
+    echo '{"alt":"on", "tooltip": "On Screen Keyboard: ON"}'
   else
-    echo '{"text":"󰌐", "tooltip": "On Screen Keyboard: OFF"}'
+    echo '{"alt":"off", "tooltip": "On Screen Keyboard: OFF"}'
   fi
 elif command -v squeekboard >/dev/null; then
   if pgrep -x squeekboard >/dev/null; then
-    echo '{"text":"󰌌", "tooltip": "On Screen Keyboard: ON"}'
+    echo '{"alt":"on", "tooltip": "On Screen Keyboard: ON"}'
   else
-    echo '{"text":"󰌐", "tooltip": "On Screen Keyboard: OFF"}'
+    echo '{"alt":"off", "tooltip": "On Screen Keyboard: OFF"}'
   fi
 fi
