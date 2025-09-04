5232ad767557053ed6873329b288ae4406b33744
wintersnowgod <git.xerox732@passinbox.com>
2025-09-04 12:37:51 +0800

    waybar improve cusom menu script

--- .config/waybar/scripts/drawer.sh
+++ .config/waybar/scripts/drawer.sh
@@ -12,3 +12,25 @@ case $1 in
     -l) launch ;;
     -k) killdrawer ;;
 esac
+
+ID=$(cat /etc/os-release | grep '^ID=' | head -n1 | cut -d= -f2 | tr -d '"')
+case "$ID" in
+    arch)
+        echo '{"alt":"arch"}'
+        ;;
+    debian)
+        echo '{"alt":"debian"}'
+        ;;
+    ubuntu)
+        echo '{"alt":"ubuntu"}'
+        ;;
+    linuxmint)
+        echo '{"alt":"mint"}'
+        ;;
+    pop)
+        echo '{"alt":"pop"}'
+        ;;
+    *)
+        echo '{"alt":"default"}'
+        ;;
+esac
