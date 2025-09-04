94b2467c976420d84f4508b3abdd4b75d75efcbc
wintersnowgod <git.xerox732@passinbox.com>
2025-09-04 12:38:58 +0800

    waybar improve osk module

--- .config/waybar/scripts/on-screen-keyboard.sh
+++ .config/waybar/scripts/on-screen-keyboard.sh
@@ -3,20 +3,16 @@ toggle() {
   if command -v wvkbd-mobintl >/dev/null; then
     if pgrep -x wvkbd-mobintl >/dev/null; then
         pkill wvkbd-mobintl
-        echo '{"alt":"off", "tooltip": "On Screen Keyboard: OFF"}'
     elif command -v wvkbd-mobintl >/dev/null; then
         wvkbd-mobintl -L 300 &
-        echo '{"alt":"on", "tooltip": "On Screen Keyboard: ON"}'
     fi
   elif command -v squeekboard >/dev/null; then
     if pgrep -x squeekboard >/dev/null; then
         pkill squeekboard
         fcitx5 &
-        echo '{"alt":"off", "tooltip": "On Screen Keyboard: OFF"}'
     elif command -v squeekboard >/dev/null; then
         pkill fcitx5
         squeekboard &
-        echo '{"alt":"on", "tooltip": "On Screen Keyboard: ON"}'
     fi
   fi
 }
@@ -25,8 +21,6 @@ case "$1" in
   -t|--toggle)
     toggle
     sleep 0.1
-    pkill -SIGRTMIN+15 waybar
-    exit 0
     ;;
 esac
 
