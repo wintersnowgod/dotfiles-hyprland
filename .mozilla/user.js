35dd7b894b2f4500e539e6880ed7888a99233feb
wintersnowgod <git.xerox732@passinbox.com>
2025-10-16 20:48:44 +0800

    add user.js rules for darkreader

--- .mozilla/user.js
+++ .mozilla/user.js
@@ -1,4 +1,7 @@
+//user_pref("",);
 user_pref("ui.key.menuAccessKey", 0);
 user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
 user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org");
-user_pref("browser.tabs.unloadOnLowMemory", true);
+// allow extensions eg:darkreader to access restricted sites eg:mozilla extensions website
+user_pref("extensions.webextensions.restrictedDomains", "");
+user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
